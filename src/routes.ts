370440f76d3b3e19d2c1f79742d6b6dcd90d37cd
Thiago Roieski <thiago@thiagomorello.com>
2024-02-12 13:18:59 -0300

    axios test

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,21 @@
 import { Router, Request, Response } from 'express'
 import blingController from './controllers/blingController'
+import axios from 'axios'
 const routes = Router()
 
-routes.get('/', (request: Request, response: Response) => {
+routes.get('/', async (request: Request, response: Response) => {
+  const ip = await axios
+    .get('https://api.ipify.org?format=json')
+    .then((response) => {
+      return response.data.ip
+    })
+    .catch(() => {
+      return 'Não foi possível obter o IP'
+    })
+
   return response.json({
     message: 'Hello World',
-    ip: request.connection.remoteAddress,
+    ip,
   })
 })
 
