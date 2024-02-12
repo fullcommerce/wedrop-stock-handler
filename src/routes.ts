a4a98f94db45c04c0b258924c55d6082150254d9
Thiago Roieski <thiago@thiagomorello.com>
2024-02-12 13:22:17 -0300

    get request on webhook site

--- src/routes.ts
+++ src/routes.ts
@@ -13,9 +13,16 @@ routes.get('/', async (request: Request, response: Response) => {
       return 'Não foi possível obter o IP'
     })
 
+  const simpleRequest = await axios
+    .get('https://webhook.site/1be97876-50c7-48af-959a-2dce1af28063')
+    .then((response) => {
+      return response.data
+    })
+
   return response.json({
     message: 'Hello World',
     ip,
+    simpleRequest,
   })
 })
 
