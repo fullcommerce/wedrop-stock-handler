6ce9e6d5f31a66c0f4db4271dd771260a0a16fad
Thiago Roieski <thiago@thiagomorello.com>
2024-02-12 13:13:03 -0300

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,10 @@ import blingController from './controllers/blingController'
 const routes = Router()
 
 routes.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'Hello World' })
+  return response.json({
+    message: 'Hello World',
+    ip: request.connection.remoteAddress,
+  })
 })
 
 routes.post('/update', blingController.update)
