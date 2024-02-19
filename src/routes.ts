e604369c3d6fb54ac32cc0a3b38f8bfb66bce2c5
Thiago Roieski <thiago@thiagomorello.com>
2024-02-19 14:50:09 -0300

    fix

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ import { ensureAuthenticateUser } from './middlewares/ensureAuthenticateUser'
 const routes = Router()
 
 routes.get('/', async (request: Request, response: Response) => {
-  const ip = await axios
+  /* const ip = await axios
     .get('https://api.ipify.org?format=json')
     .then((response) => {
       return response.data.ip
@@ -19,12 +19,10 @@ routes.get('/', async (request: Request, response: Response) => {
     .get('https://webhook.site/1be97876-50c7-48af-959a-2dce1af28063')
     .then((response) => {
       return response.data
-    })
+    }) */
 
   return response.json({
     message: 'Hello World',
-    ip,
-    simpleRequest,
   })
 })
 
