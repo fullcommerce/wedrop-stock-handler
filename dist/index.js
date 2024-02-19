0739495d7487496319c769fac76638b8102366ed
Thiago Roieski <thiago@thiagomorello.com>
2024-02-19 15:20:11 -0300

    fix cors error

--- dist/index.js
+++ dist/index.js
@@ -9,11 +9,11 @@ const dotenv_1 = require("dotenv");
 const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
-app.use(cors_1.default);
+app.use((0, cors_1.default)());
 app.use(express_1.default.json({
     limit: '200mb',
 }));
-app.use((err, request, response) => {
+app.use((err, request, response, next) => {
     if (err instanceof Error) {
         return response.status(400).json({
             error: err.message,
