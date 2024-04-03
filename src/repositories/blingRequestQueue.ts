488d9debd0324f13bb0297dbc5e2b511954e8df5
Thiago Roieski <thiago@thiagomorello.com>
2024-04-03 09:48:47 -0300

    test

--- src/repositories/blingRequestQueue.ts
+++ src/repositories/blingRequestQueue.ts
@@ -2,11 +2,11 @@ import PQueue from 'p-queue'
 export const blingRequestQueue = new PQueue({
   interval: 1000,
   intervalCap: 2,
-  concurrency: 2,
+  concurrency: 1,
 })
 
 export const blingV2RequestQueue = new PQueue({
   interval: 1000,
-  intervalCap: 1,
-  concurrency: 3,
+  intervalCap: 3,
+  concurrency: 1,
 })
